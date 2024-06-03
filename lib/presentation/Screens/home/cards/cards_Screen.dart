250be9e1d394ff75efa1bc5caea4a9b9dfb6a03c
IsraelMerlyn <merlyn142a@gmail.com>
2024-06-03 15:05:30 -0600

    tasks #0.0.6 Agregando pages Button and Cards

---
+++ lib/presentation/Screens/home/cards/cards_Screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class CardsSCreen extends StatelessWidget {
+  const CardsSCreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Buttons"),
+        
+      ),
+      body: Placeholder(),
+    );
+  }
+}
